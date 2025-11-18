6b98abfc132effc56b97b001e48c3eb4229f2b58
Andy Quach <andy.quach@richmond.edu>
2025-11-18 14:32:21 -0500

    health bars

--- DeathRunnersGodotPrototype/PrototypeFiles/hazard.gd
+++ DeathRunnersGodotPrototype/PrototypeFiles/hazard.gd
@@ -1,12 +1,9 @@
 extends RigidBody2D
 
-func _ready() -> void:
-	contact_monitor = true
-	max_contacts_reported = 4
+@export var damage: float = 30.0
 
 func _on_body_entered(body: Node) -> void:
-	# Only care if we hit the player
 	if body.is_in_group("player"):
-		if body.has_method("hit_by_hazard"):
-			body.hit_by_hazard()
-		queue_free()  # remove this hazard after hit
+		if body.has_method("apply_damage"):
+			body.apply_damage(damage)
+		queue_free()
