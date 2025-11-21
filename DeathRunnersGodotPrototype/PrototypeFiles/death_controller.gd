98bf7bce334fdb97abbedd5488ebbb2995e9a65c
Andy Quach <andy.quach@richmond.edu>
2025-11-21 12:44:50 -0500

    change falling block

--- DeathRunnersGodotPrototype/PrototypeFiles/death_controller.gd
+++ DeathRunnersGodotPrototype/PrototypeFiles/death_controller.gd
@@ -1,19 +1,35 @@
 extends Node2D
 
 @export var survivor_path: NodePath
+@export var hazard_scene: PackedScene  # Assign hazard.tscn in inspector
 
 var survivor: Node2D
+var falling_block_cooldown: float = 0.0
+var falling_block_max_cooldown: float = 2.0  # 2 second cooldown
 
 func _ready() -> void:
 	if survivor_path != NodePath():
 		survivor = get_node(survivor_path)
 	else:
 		survivor = null
+	
+	# Load hazard scene if not assigned
+	if hazard_scene == null:
+		hazard_scene = load("res://PrototypeFiles/hazard.tscn")
+	
 	print("DeathController ready, survivor = ", survivor)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if survivor == null:
 		return
+	
+	# Update cooldown
+	if falling_block_cooldown > 0:
+		falling_block_cooldown -= delta
+
+	# Spawn falling block above player
+	if Input.is_action_just_pressed("death_spawn_falling"):
+		_spawn_falling_block()
 
 	if Input.is_action_just_pressed("death_spawn_boulder"):
 		_activate_next_trap_ahead("trap_boulder")
@@ -21,12 +37,31 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("death_aoe"):
 		_activate_next_trap_ahead("trap_aoe")
 
-	if Input.is_action_just_pressed("death_spawn_falling"):
-		_activate_next_trap_ahead("trap_falling")
-
 	if Input.is_action_just_pressed("death_arrow"):
 		_activate_next_trap_ahead("trap_arrow")
 
+func _spawn_falling_block() -> void:
+	if falling_block_cooldown > 0:
+		print("Falling block on cooldown: ", falling_block_cooldown)
+		return
+	
+	if hazard_scene == null:
+		print("ERROR: hazard_scene not loaded!")
+		return
+	
+	# Spawn above player
+	var hazard = hazard_scene.instantiate()
+	get_tree().current_scene.add_child(hazard)
+	
+	# Position it above the player
+	var spawn_height = 200  # pixels above player
+	hazard.global_position = Vector2(
+		survivor.global_position.x,
+		survivor.global_position.y - spawn_height
+	)
+	
+	falling_block_cooldown = falling_block_max_cooldown
+	print("Falling block spawned at: ", hazard.global_position)
 
 func _activate_next_trap_ahead(group_name: String) -> void:
 	var traps: Array = get_tree().get_nodes_in_group(group_name)
